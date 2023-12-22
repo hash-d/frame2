205df7a849abd2cf635a3d4eaadadf3fb8a4c4dc
Danilo Gonzalez Hashimoto <97529980+hash-d@users.noreply.github.com>
2023-12-22 01:46:40 -0300

    fixed func-fail test
    Before, it was always failing.  With the introduction of validate.Phase,
    it is now possible to properly test func-fail and return a success when
    it properly sees the function failing

--- pkg/execute/function_test.go
+++ pkg/execute/function_test.go
@@ -1,4 +1,4 @@
-package execute
+package execute_test
 
 import (
 	"errors"
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	frame2 "github.com/hash-d/frame2/pkg"
+	"github.com/hash-d/frame2/pkg/execute"
+	"github.com/hash-d/frame2/pkg/validate"
 )
 
 func TestFunction(t *testing.T) {
@@ -14,7 +16,7 @@ func TestFunction(t *testing.T) {
 		MainSteps: []frame2.Step{
 			{
 				Name: "func-ok",
-				Modify: Function{
+				Modify: execute.Function{
 					Fn: func() error {
 						log.Printf("Hello")
 						return nil
@@ -22,14 +24,22 @@ func TestFunction(t *testing.T) {
 				},
 			}, {
 				Name: "func-fail",
-				Modify: Function{
-					Fn: func() error {
-						return errors.New("failed!")
+				Validator: validate.Phase{
+					// We need to use validate.Phase to capture the Modify error as a
+					// validation step: Modify is supposed to always generate an actual
+					// error, and ExpectError is only for validations.
+					Phase: frame2.Phase{
+						MainSteps: []frame2.Step{
+							{
+								Modify: execute.Function{
+									Fn: func() error {
+										return errors.New("failed!")
+									},
+								},
+							},
+						},
 					},
 				},
-				// This does not help, as it allows only for
-				// validation errors; modify steps are not expected
-				// to fail
 				ExpectError: true,
 			},
 		},
