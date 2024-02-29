7985b5ff93ed98660d8281b43f95621aa0524700
Danilo Gonzalez Hashimoto <97529980+hash-d@users.noreply.github.com>
2024-02-28 21:56:56 -0300

    Added ValidatorsRetry, moved to subpackage, others
    - package move was to avoid cyclic dependencies
    - addressed cancellation of contexts
    - properly implemented 'combo'
    - now using validatorSubFinal, so disruptors can be done for each
      subtest
    - Added GetPhase(), for more natural use

--- pkg/subrunner/effect.go
+++ pkg/subrunner/effect.go
@@ -1,8 +1,11 @@
-package frame2
+package subrunner
 
 import (
+	"context"
 	"fmt"
 
+	frame2 "github.com/hash-d/frame2/pkg"
+	"github.com/hash-d/frame2/pkg/execute"
 	"github.com/imdario/mergo"
 )
 
@@ -17,9 +20,11 @@ const (
 
 // On each cycle executed by Effects, its BaseFrame is patched
 // with the Patch below, and the Validators ensue
-type CauseEffect[T Executor] struct {
-	Patch      T
-	Validators []Validator
+type CauseEffect[T frame2.Executor] struct {
+	Doc             string
+	Patch           T
+	Validators      []frame2.Validator
+	ValidatorsRetry frame2.RetryOptions
 }
 
 // A generic cause-effect verifier
@@ -30,12 +35,16 @@ type CauseEffect[T Executor] struct {
 //
 // The Setup is per-cycle.  If additional, multi-cycle setup is
 // required, it's up to the enclosing test to provide it.
-type Effects[T Executor] struct {
+type Effects[T frame2.Executor, PT interface {
+	*T
+	Execute() error
+}] struct {
 	Name      string
-	BaseFrame T
-	Setup     []Step
+	Doc       string
+	BaseFrame PT
+	Setup     []frame2.Step
 	Effects   map[string]CauseEffect[T]
-	TearDown  []Step
+	TearDown  []frame2.Step
 
 	// Each combo contains a list of Effects.  After all
 	// of their patches are applied to it, the BaseFrame
@@ -46,63 +55,73 @@ type Effects[T Executor] struct {
 }
 
 // Stepper
-func (e *Effects[T]) GetStep() *Step {
-	var s *Step
+func (e *Effects[T, PT]) GetStep() (*frame2.Step, context.CancelFunc) {
+	var s *frame2.Step
+	var cancel context.CancelFunc
 	switch e.ExecutionProfile {
 	case COMBO:
-		s = e.getComboStep()
+		s, cancel = e.getComboStep()
 	case INDIVIDUAL:
 		s = e.getIndividualStep()
 	case BOTH:
-		s = e.getBothStep()
+		s, cancel = e.getBothStep()
 	default:
 		panic(fmt.Sprintf("no such ExecutionProfile: %v", e.ExecutionProfile))
 	}
-	return s
+	return s, cancel
 }
 
-func (e Effects[T]) getComboStep() *Step {
-	s := Step{
+func (e Effects[T, PT]) getComboStep() (*frame2.Step, context.CancelFunc) {
+	var cancel context.CancelFunc
+	s := frame2.Step{
 		Name: e.Name,
+		Doc:  e.Doc,
 	}
 	for name, effects := range e.Combos {
-		frame := e.BaseFrame
-		validators := []Validator{}
+		frame := *e.BaseFrame
+		validators := []frame2.Validator{}
+		opt := frame2.RetryOptions{}
 		for _, effect := range effects {
 			error := mergo.Merge(&frame, e.Effects[effect].Patch)
 			if error != nil {
 				panic("error merging structs")
 			}
 			validators = append(validators, e.Effects[effect].Validators...)
+			opt, cancel = opt.Max(e.Effects[effect].ValidatorsRetry)
 		}
-		sub := Step{
-			Name:       name,
-			Modify:     frame,
-			Validators: validators,
+		sub := frame2.Step{
+			Name:              name,
+			Modify:            PT(&frame),
+			Validators:        validators,
+			ValidatorSubFinal: true,
+			ValidatorRetry:    opt,
 		}
 		s.Substeps = append(s.Substeps, &sub)
 	}
-	return &s
+	return &s, cancel
 }
 
-func (e Effects[T]) getIndividualStep() *Step {
-	s := Step{
+func (e Effects[T, PT]) getIndividualStep() *frame2.Step {
+	s := frame2.Step{
 		Name: e.Name,
+		Doc:  e.Doc,
 	}
 
 	for name, effect := range e.Effects {
-		frame := e.BaseFrame
-		error := mergo.Merge(&frame, effect.Patch)
-		if error != nil {
-			panic("error merging structs")
+		frame := *e.BaseFrame
+		err := mergo.Merge(&frame, effect.Patch)
+		if err != nil {
+			panic(fmt.Sprintf("error merging structs (%s)", err))
 		}
-		sub := Step{
+		sub := frame2.Step{
 			Name: name,
-			Modify: Phase{
-				MainSteps: []Step{
+			Modify: frame2.Phase{
+				MainSteps: []frame2.Step{
 					{
-						Modify:     frame,
-						Validators: effect.Validators,
+						Modify:            PT(&frame),
+						Validators:        effect.Validators,
+						ValidatorSubFinal: true,
+						ValidatorRetry:    effect.ValidatorsRetry,
 					},
 				},
 				Teardown: e.TearDown,
@@ -116,9 +135,35 @@ func (e Effects[T]) getIndividualStep() *Step {
 
 // Returns a step with substeps for both Combo and Individual
 // tests (in that order)
-func (e Effects[T]) getBothStep() *Step {
-	s := e.getComboStep()
+func (e Effects[T, PT]) getBothStep() (*frame2.Step, context.CancelFunc) {
+	s, cancel := e.getComboStep()
 	i := e.getIndividualStep()
 	s.Substeps = append(s.Substeps, i.Substeps...)
-	return s
+	return s, cancel
+}
+
+func (e Effects[T, PT]) GetPhase(runner *frame2.Run) frame2.Phase {
+	steps, cancel := e.GetStep()
+	phase := frame2.Phase{
+		Runner: runner,
+		MainSteps: []frame2.Step{
+			*steps,
+		},
+	}
+	// We cannot defer cancel(), as we're just returning a phase, not
+	// running it.  The context cancellation needs to be done after
+	// the steps are actually executed, so we add that as a step to
+	// the returned phase
+	if cancel != nil {
+		phase.MainSteps = append(phase.MainSteps, frame2.Step{
+			Doc: "cancel merged context",
+			Modify: &execute.Function{
+				Fn: func() error {
+					cancel()
+					return nil
+				},
+			},
+		})
+	}
+	return phase
 }
