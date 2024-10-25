1550e42fcf1607177f0f8ecaa777cbe7d4a4f892
Danilo Gonzalez Hashimoto <97529980+hash-d@users.noreply.github.com>
2024-10-25 17:06:48 -0300

    Use env's TEST_ID if available

--- pkg/init.go
+++ pkg/init.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"log"
+	"os"
 
 	"github.com/google/uuid"
 )
@@ -11,19 +12,27 @@ import (
 // Populate the package variables id and smallId
 func init() {
 
-	_id, err := uuid.NewUUID()
-	if err != nil {
-		_id = uuid.New()
+	// By default, we use the environment provided TEST_ID
+	stringId := os.Getenv("TEST_ID")
+	if stringId == "" {
+		// If that was not provided, we create a new one, but each
+		// package/binary will have its own, on a single `go test` invokation
+		uu, err := uuid.NewUUID()
+		if err != nil {
+			uu = uuid.New()
+		}
+		stringId = uu.String()
 	}
-	hash := md5.Sum([]byte(_id.String()))
+	hash := md5.Sum([]byte(stringId))
 	asS := hex.EncodeToString(hash[:])
 
-	id = _id.String()
+	// set package-level vars
+	id = stringId
 	shortId = asS[:3]
 
 	log.Printf(
 		"This frame2 execution is identified with UUID %v and partial MD5 %v",
-		id,
+		stringId,
 		shortId,
 	)
 
