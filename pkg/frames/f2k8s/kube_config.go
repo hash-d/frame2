46579c4d4c2a1b680a99c22103c31df832ca97ad
Danilo Gonzalez Hashimoto <97529980+hash-d@users.noreply.github.com>
2024-10-26 14:12:51 -0300

    More skupper code extraction, which required some additional refactoring

--- pkg/frames/f2k8s/kube_config.go
+++ pkg/frames/f2k8s/kube_config.go
@@ -21,6 +21,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
+var parameterCodec = runtime.NewParameterCodec(scheme)
 
 var clusters = []*KubeConfig{}
 var domainClusters = map[ClusterType][]*KubeConfig{}
